dd83729acb1222e7a4c4d45ead7c7a7eb93f7603
yxyinxiao07 <1874574983@qq.com>
2019-10-21 15:19:02 +0800

    页面配置

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <template>
-    <keep-alive>
-        <router-view/>
-    </keep-alive>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
 </template>
 
-<script>
-export default {
-    // watch: {
-    //     $route (to) {
-    //         console.log(to.path);
-    //     }
-    // }
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
-</script>
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
